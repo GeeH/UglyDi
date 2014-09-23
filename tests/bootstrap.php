e7920d1f253cef62103016f00b1f299b21fe49d8
Gary Hockin <gary@hock.in>
2014-09-23 08:48:11 +0100

    Surpress warning for missing include file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-$loader = include(realpath('./../../vendor/autoload.php'));
+$loader = @include(realpath('./../../vendor/autoload.php'));
 
 if (!$loader && !($loader = @include 'vendor/autoload.php')) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
